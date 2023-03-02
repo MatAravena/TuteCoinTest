6451ec48858a0c82dde3bdb297d9594ce43f8001
Matu Aravena <matias.aravena.g@gmail.com>
2023-03-02 21:14:23 +0100

    a lot of changes

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,20 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+ require('dotenv').config();
+ require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
   solidity: "0.7.3",
+  defaultNetwork: "ropsten",
+  networks: {
+
+    
+     hardhat: {},
+     ropsten: {
+        url: API_URL,
+        accounts: [`0x${PRIVATE_KEY}`]
+     }
+  },
 };
